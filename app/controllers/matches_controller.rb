ef7c99bfe586064a2c4b54ce630216d5e993e0ef
fredalad <nick.manfreda885@gmail.com>
2016-09-23 17:32:00 -0600

    time testing9

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -25,23 +25,25 @@ class MatchesController < ApplicationController
 
     #end
     #Time.zone = "UTC"
-      Time.use_zone("Eastern Time (US \& Canada)") {
-      time_entered = Time.new(Time.zone.now.year.to_i,
-                              Time.zone.now.month.to_i,
-                              23,
-                              @match.hours.to_i,
-                              @match.minutes.to_i)
-      @match.acpt_team_name = Time.zone
-      the_offset = DateTime.parse(time_entered.to_s).in_time_zone(current_user.time_zone).utc_offset
-      @match.acpt_team_wins = the_offset
+      the_offset = DateTime.parse(@match.hours.to_s + ":" + @match.minutes.to_s, '%H:%M').in_time_zone(current_user.time_zone).utc_offset
 
-      new_time = Time.new(Time.zone.now.year.to_i,
-                          Time.zone.now.month.to_i,
-                          23,
-                          @match.hours.to_i,
-                          @match.minutes.to_i, 0, the_offset)
-      @match.match_time = new_time
-}
+      # Time.use_zone("Eastern Time (US \& Canada)") {
+      # time_entered = Time.new(Time.zone.now.year.to_i,
+      #                         Time.zone.now.month.to_i,
+      #                         23,
+      #                         @match.hours.to_i,
+      #                         @match.minutes.to_i)
+      # @match.acpt_team_name = Time.zone
+      # the_offset = DateTime.parse(time_entered.to_s).in_time_zone(current_user.time_zone).utc_offset
+      # @match.acpt_team_wins = the_offset
+
+      # new_time = Time.new(Time.zone.now.year.to_i,
+      #                     Time.zone.now.month.to_i,
+      #                     23,
+      #                     @match.hours.to_i,
+      #                     @match.minutes.to_i, 0, the_offset)
+      # @match.match_time = new_time
+#}
 
     @match.acpt_team_wins = Time.strptime(@match.hours.to_s + ":" + @match.minutes + ":" + current_user.time_zone ,'%H:%M:%Z').utc_offset
     @match.match_time = Time.strptime(@match.hours.to_s + ":" + @match.minutes + ":" + current_user.time_zone ,'%H:%M:%Z')
